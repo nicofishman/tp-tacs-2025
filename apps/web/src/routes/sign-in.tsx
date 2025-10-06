56b8a8f706273a341020d1b23da8d5c039f3aa5a
nicofishman <nicourifishman@gmail.com>
2025-10-06 20:40:18 -0300

    feat: if signed in, redirect from auth pages

--- apps/web/src/routes/sign-in.tsx
+++ apps/web/src/routes/sign-in.tsx
@@ -1,6 +1,18 @@
+import { useAuth } from "@web/components/auth-provider";
 import SignInForm from "@web/components/sign-in-form";
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
 
 export default function SignIn() {
+  const { user } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (user) {
+      navigate("/", { replace: true });
+    }
+  }, [user, navigate]);
+
   return (
     <div className="flex min-h-full items-center justify-center px-4 py-12 sm:px-6 lg:px-8">
       <SignInForm />
