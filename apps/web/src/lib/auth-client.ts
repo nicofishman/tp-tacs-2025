ee220e3fbb20bf1a8c4542dd1030b1ffb2a88f5d
Nicolás Fishman <nicourifishman@gmail.com>
2025-10-27 23:03:20 -0300

    aversiandas

--- apps/web/src/lib/auth-client.ts
+++
@@ -1,9 +0,0 @@
-import { createAuthClient } from "better-auth/react";
-
-const apiUrl = import.meta.env.SSR
-  ? process.env.API_URL
-  : import.meta.env.VITE_API_URL;
-
-export const authClient = createAuthClient({
-  baseURL: apiUrl || "http://localhost:3000",
-});
--- apps/web/src/lib/fetch.ts
+++ apps/web/src/lib/fetch.ts
@@ -5,7 +5,7 @@ const apiUrl = import.meta.env.SSR
   ? process.env.API_URL
   : import.meta.env.VITE_API_URL;
 
-export const api = treaty<App>(apiUrl || "http://localhost:3000", {
+export const api = treaty<App>(apiUrl ?? "http://localhost:3000", {
   fetch: {
     credentials: "include",
   },
