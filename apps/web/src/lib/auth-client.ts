ce7de0c479edb35bd7c09703782a7c5c31f4b993
Nicolás Fishman <nicourifishman@gmail.com>
2025-10-27 22:30:37 -0300

    fix: auth url

--- apps/web/src/lib/auth-client.ts
+++ apps/web/src/lib/auth-client.ts
@@ -1,5 +1,9 @@
 import { createAuthClient } from "better-auth/react";
 
+const apiUrl = import.meta.env.SSR
+  ? process.env.API_URL
+  : import.meta.env.VITE_API_URL;
+
 export const authClient = createAuthClient({
-  baseURL: import.meta.env.VITE_SERVER_URL || "http://localhost:3000",
+  baseURL: apiUrl || "http://localhost:3000",
 });
